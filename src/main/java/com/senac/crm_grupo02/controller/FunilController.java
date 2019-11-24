a1146a84d7ed99c366aba192f0c74c496a7e183f
Rodrigo Moreira <rodrigomoreira664@gmail.com>
2019-11-24 15:57:20 -0300

    Ajuste Kanban

--- src/main/java/com/senac/crm_grupo02/controller/FunilController.java
+++ src/main/java/com/senac/crm_grupo02/controller/FunilController.java
@@ -7,18 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.senac.crm_grupo02.service.OfertaService;
+import com.senac.crm_grupo02.service.ProdutoService;
 
 @Controller
 @RequestMapping("funil")
 public class FunilController {
 	
 	@Autowired
-	OfertaService servicoOferta;	
+	ProdutoService servicoProduto;
 	
 	@GetMapping("/listarFunilVendas")
 	public ModelAndView paginaPrincipal() {
 		ModelAndView mv = new ModelAndView("paginas/funil/funilDeVendas");
-		mv.addObject("ofertaLista", servicoOferta.searchAll());
+		mv.addObject("produtoLista", servicoProduto.searchAll());
 		return mv;
 	}
 
--- src/main/java/com/senac/crm_grupo02/controllerREST/OfertaControllerREST.java
+++ src/main/java/com/senac/crm_grupo02/controllerREST/OfertaControllerREST.java
@@ -1,8 +1,14 @@
 package com.senac.crm_grupo02.controllerREST;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.senac.crm_grupo02.domain.Oferta;
+import com.senac.crm_grupo02.domain.Produto;
 import com.senac.crm_grupo02.service.OfertaService;
 import com.senac.crm_grupo02.service.ProdutoService;
 
@@ -43,5 +50,20 @@ public class OfertaControllerREST {
 		
         return ofertaNova;
     }
+	
+	@RequestMapping(value = "/obterOferta", method = RequestMethod.GET)
+	public ResponseEntity<List<Oferta>> obterOferta() throws ObjectNotFoundException {
+		
+		List<Oferta> listaOferta = new ArrayList<Oferta>();
+		listaOferta = servicoOferta.searchAll();		
+		Map<Integer, Oferta> listaOfertahas = new HashMap<Integer, Oferta>();
+		int cont = 1;
+		
+		for (Oferta itemOferta : listaOferta) {							
+			listaOfertahas.put(cont, itemOferta);	
+			cont++;							
+		}
+		return new ResponseEntity<List<Oferta>>(new ArrayList<Oferta>(listaOfertahas.values()), HttpStatus.OK);		
+	}
 
 }
