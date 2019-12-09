586af468052072866aa4ef4961e73f500478b5e7
Rodrigo Moreira <rodrigomoreira664@gmail.com>
2019-12-08 22:26:56 -0300

    adicao da funcionalidade do chatbot

--- src/main/java/com/senac/crm_grupo02/controller/DashBoardController.java
+++ src/main/java/com/senac/crm_grupo02/controller/DashBoardController.java
@@ -1,18 +1,24 @@
 package com.senac.crm_grupo02.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.senac.crm_grupo02.service.EtapaFunilService;
+
 @Controller
 @RequestMapping("dashboard")
 public class DashBoardController {
 	
+	@Autowired
+	EtapaFunilService servicoEtapaFunil;
+	
 	@GetMapping("/dashboard")
 	public ModelAndView paginaPrincipal() {
 		ModelAndView mv = new ModelAndView("paginas/dashboard/dashboardRelatorios");
-		//mv.addObject("clientes", servicoCliente.searchAll());
+		mv.addObject("ofertasPorEtapa", servicoEtapaFunil.searchAll());
 		return mv;
 	}
 
